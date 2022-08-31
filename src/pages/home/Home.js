03b2db031e96089fb52b141d5f93e9fe41a0f2d5
nauval yusuf addairy <nauvalyusufad@gmail.com>
2022-08-31 21:11:24 +0700

    re enter de homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,21 +5,6 @@ import Product from "../../components/product/Product";
 import Products from "../../components/product/Products";
 
 const Home = () => {
-  const Box = styled.div`
-    width: 200px;
-    height: 100px;
-    padding: 5px;
-    p {
-      display: -webkit-box;
-      -webkit-box-orient: vertical;
-      text-overflow: ellipsis;
-      -webkit-line-clamp: 2;
-      line-clamp: 2;
-      overflow: hidden;
-      white-space: pre-wrap;
-      word-break: break-all;
-    }
-  `;
   return (
     <div>
       <Navbar />
